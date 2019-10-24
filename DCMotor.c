04a8fe6858e2c379ed3a66ae5faa7e4c581e282c
MLBY <geekmlby@163.com>
2019-10-24 14:19:36 +0800

    加入了传感器的代码，现在能做到放地上就能走，拿起来就停。在下载程序的时候，要把P30和P31两个口的线拔出，否则下载不了。

--- DCMotor.c
+++ DCMotor.c
@@ -3,10 +3,6 @@
 #define uchar unsigned char  
 #define uint  unsigned int  
 
-uint highN1;
-uint lowN1;
-uint highN2;
-uint lowN2;
 uint cycleN;
 uint count;
 sbit pwm1 = P0^0;          	//PWM信号输出
@@ -19,22 +15,22 @@ void Sys_Init()
 	TMOD = 0x10;			//定时器1做为8位计数器
 	IE = 0x88;
 
-	TH1 = 0xFC;
+	TH1 = 0xfc;
 	TL1 = 0x66;
 	TR1 = 1;
+
+	P3 = 0xff;
 }
 
 void Var_Init()
 {	
-	highN1 = 0;
-	lowN1 = 200;
-	highN2 = 250;
-	lowN2 = 450;
 	cycleN = 6;
 	count = 0;
 
 	pwm1 = 1;
 	pwm2 = 0;
+	pwm3 = 1;
+	pwm4 = 0;
 }
 
 main()
@@ -44,14 +40,13 @@ main()
 
  	while(1)
 	{
-		/*pwm1 = 1;
-		pwm2 = 0;*/
+
 	}
 }
 
 void Timer1_Int() interrupt 3 	//中断程序
 {
-	TH1 = 0xFC;
+	TH1 = 0xfc;
 	TL1 = 0x66;
 
 	count++;
@@ -59,33 +54,19 @@ void Timer1_Int() interrupt 3 	//中断程序
 	{
 		count = 0;
 	}
-	/*if(count >= highN1 && count < lowN1)
+
+	if(count >= 0 && count < 3 && P3 != 0xff)
 	{
 		pwm1 = 1;
 		pwm2 = 0;
-	}
-	if((count >= lowN1 && count < highN2) || (count >= lowN2 && count < cycleN))
-	{
-		pwm1 = 0;
-		pwm2 = 0;
-	}
-	if(count >= highN2 && count < lowN2)
-	{
-		pwm1 = 0;
-		pwm2 = 1;
-	}*/
-	if(count >= 0 && count < 3)
-	{
-		pwm1 = 1;
-		pwm2 = 0;
-		pwm3 = 0;
+		pwm3 = 1;
 		pwm4 = 0;
 	}
 	else
 	{
 		pwm1 = 0;
 		pwm2 = 0;
-		pwm3 = 1;
+		pwm3 = 0;
 		pwm4 = 0;
 	}
 }
